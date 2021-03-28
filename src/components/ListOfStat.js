1e25dbadc9faf6e78b5d0e0d05ddd19d2d3cb8ed
cristicretu <cristicretu38@gmail.com>
2021-03-28 19:37:58 +0300

    f

--- src/components/ListOfStat.js
+++ src/components/ListOfStat.js
@@ -11,5 +11,6 @@ export default function ListOfStat() {
         );
       })}
     </div>
+
   )
 }
--- src/constant/index.js
+++ src/constant/index.js
@@ -20,6 +20,11 @@ const report = {
 			value: 100,
 			delta: 20,
 		},
+		{
+			label: 'Vaccine doses administered',
+			value: 100,
+			delta: 20,
+		},
 	],
 };
 
@@ -28,13 +33,15 @@ const labelColor = [
 	'text-blue-600',
 	'text-green-700',
 	'text-gray-500',
+	'text-pink-500'
 ];
 
 const bgColor = [
-	'transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 h-15 w-15 items-center p-2 flex bg-gray-800 bg-opacity-50 hover:bg-red-700 hover:bg-opacity-30 rounded',
-	'transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 h-15 w-15 items-center p-2 flex bg-gray-800 bg-opacity-50 hover:bg-blue-700 hover:bg-opacity-30 rounded',
-	'transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 h-15 w-15 items-center p-2 flex bg-gray-800 bg-opacity-50 hover:bg-green-800 hover:bg-opacity-30 rounded',
-	'transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 h-15 w-15 items-center p-2 flex bg-gray-800 bg-opacity-50 hover:bg-gray-600 hover:bg-opacity-30 rounded ',
+	'transition duration-500 ease-in-out transform hover:-translate-y-0.5 hover:scale-105 h-15 w-15 items-center p-2 flex bg-gray-800 bg-opacity-50 hover:bg-red-700 hover:bg-opacity-30 rounded',
+	'transition duration-500 ease-in-out transform hover:-translate-y-0.5 hover:scale-105 h-15 w-15 items-center p-2 flex bg-gray-800 bg-opacity-50 hover:bg-blue-700 hover:bg-opacity-30 rounded',
+	'transition duration-500 ease-in-out transform hover:-translate-y-0.5 hover:scale-105 h-15 w-15 items-center p-2 flex bg-gray-800 bg-opacity-50 hover:bg-green-800 hover:bg-opacity-30 rounded',
+	'transition duration-500 ease-in-out transform hover:-translate-y-0.5 hover:scale-105 h-15 w-15 items-center p-2 flex bg-gray-800 bg-opacity-50 hover:bg-gray-600 hover:bg-opacity-30 rounded ',
+	'flex col-span-2 transition duration-500 ease-in-out transform hover:-translate-y-0.5 hover:scale-105 h-15 w-15 items-center p-2  bg-gray-800 bg-opacity-50 hover:bg-pink-600 hover:bg-opacity-30 rounded',
 ];
 
 export { labelColor, report, bgColor };
